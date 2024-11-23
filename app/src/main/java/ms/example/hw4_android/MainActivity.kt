5ccb35247e20eebb71ec0bf036859570a7dcca3d
Nati <tusikf@yandex.ru>
2024-11-23 17:16:20 +0300

    done

--- app/src/main/java/ms/example/hw4_android/MainActivity.kt
+++ app/src/main/java/ms/example/hw4_android/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.switchmaterial.SwitchMaterial
 import ms.example.hw4_android.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -22,22 +23,31 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         var count = 0
+        var countradio = 0
         val a = Random.nextInt(101)
         binding.progress1.progress = a
-        binding.count1.text = a.toString()
-        binding.switch1.isChecked = true
+        binding.count1.text = countradio.toString()
+        binding.switch1.isChecked = false
+        binding.chek1.isEnabled = false
+        binding.chek2.isEnabled = false
+        binding.button1.isEnabled = false
+
 
         binding.switch1.setOnCheckedChangeListener { compoundButton, b ->
             if (b){
                 // when switch button is checked
                 binding.chek1.isEnabled = true
                 binding.chek2.isEnabled = true
-                Toast.makeText(this, "что-то работает", Toast.LENGTH_SHORT).show()
-                binding.chek1.setOnClickListener {
-                    if (binding.chek1.isChecked) count++
+                Toast.makeText(this, "выбрать обязательно какой-то вариант", Toast.LENGTH_SHORT).show()
+                binding.chek1.setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        count++
+                    }
                 }
-                binding.chek2.setOnClickListener {
-                    if (binding.chek2.isChecked) count++
+                binding.chek2.setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        count++
+                    }
                 }
             }
             else{
@@ -46,39 +56,43 @@ class MainActivity : AppCompatActivity() {
                 binding.chek2.isEnabled = false
                 count = -5
             }
+            if (checkSave(binding, countradio, count)) binding.button1.isEnabled = true
         }
-        binding.button1.isEnabled = false
-        
-        binding.nameVvod.setOnClickListener {
-            if (binding.nameVvod.isActivated) {
-                count++
-                Toast.makeText(this, "имя сработало2", Toast.LENGTH_SHORT).show()
-            }
-            val bb = binding.name.text
-            if (bb != null) {
-                count++
-                Toast.makeText(this, "имя сработало", Toast.LENGTH_SHORT).show()
-            }
+
+        binding.name.doOnTextChanged { text, _, _, _ ->
+            if (checkSave(binding, countradio, count)) binding.button1.isEnabled = true
+
         }
         binding.telefone.setOnClickListener {
-            if (binding.telefone.isEnabled) count++
+            if (checkSave(binding, countradio, count)) binding.button1.isEnabled = true
         }  
-        binding.radioGroup.setOnCheckedChangeListener { radioGroup, i ->
-            when (i) {
-                0 -> count++
-                1 -> count++
-            }
+        binding.radioGroup.setOnCheckedChangeListener { _, _ ->
+            countradio++
+            Toast.makeText(this, "нажато", Toast.LENGTH_SHORT).show()
+            if (checkSave(binding, countradio, count)) binding.button1.isEnabled = true
         }
 
-
         binding.button1.setOnClickListener {
-            if (count > 1) {
-                binding.button1.isEnabled = true
-            }
             Toast.makeText(this, "Информация сохранена", Toast.LENGTH_SHORT).show()
         }
 
     }
 
+    private fun checkSave(binding: ActivityMainBinding, radio: Int, check:Int): Boolean {
+        var result = 0
+        if (binding.name.text != null) {
+            if (binding.telefonvvod.text != null) {
+                if (radio > 0) {
+                    if (check > 0) result = 1
+                }
+            }
+        }
+        else {
+            result = 0
+        }
+        if (result>0) return true
+        else return false
+
+    }
 
 }
